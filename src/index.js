0f44b126eb3ab0a77679c40579e0d86976fcc2c9
avinash <avinashsharma17012002@gmail.com>
2024-11-30 09:30:48 +0530

    api classes *

--- src/index.js
+++ src/index.js
@@ -1,34 +1,20 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import express from "express";
+import { app } from "./app.js";
 
 dotenv.config({
  path: './env'
 })
 
-connectDB();
-
-const app = express()
-
-
-
-
-/*
-(async () => {
- try {
-  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-  app.on("error", (error) => {
-   console.log("error", error)
-   throw error
-  })
-
-  app.listen(process.env.PORT () => {
-
-   console.log(`App is listening on port ${process.env.PORT}`)
-  })
- } catch (error) {
-  console.error("error: ", error)
-  throw err
- }
-
-})() */
+connectDB()
+.then(() => {
+ app.listen(process.env.PORT || 8000, () => {
+  console.log(`server is listening ${process.env.PORT}`)
+ })
+ app.on("error", (error) => {
+console.log("error", error)
+ })
+})
+.catch((err) => {
+ console.log(err)
+})
